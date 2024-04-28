16770c42d54c99efb9fa6d3a89176e1773ce9989
Irtiza Hafiz <irtizahafiz07@protonmail.com>
2024-04-27 23:37:39 -0400

    allow co reqs

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ OPENAI_EMBEDDINGS_URL = "https://api.openai.com/v1/embeddings"
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
