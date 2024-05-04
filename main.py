7ade86baad73cf85fba5f936ab30fa0273821157
Irtiza Hafiz <irtizahafiz07@protonmail.com>
2024-05-03 21:39:12 -0400

    modify embeddings endpoint to support blog and youtube

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from functools import lru_cache
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 # Access environment variables
 load_dotenv()
@@ -14,9 +15,16 @@ db_pass_secret = os.environ.get("db_pass_secret")
 db_host_secret = os.environ.get("db_host_secret")
 db_port_secret = os.environ.get("db_port_secret")
 open_ai_api_key_secret = os.environ.get("open_ai_api_key_secret")
+youtube_api_key = os.environ.get("youtube_api_key")
 
 DIRECTORY_PATH = "../portfolio-website-nextjs/posts/"
 OPENAI_EMBEDDINGS_URL = "https://api.openai.com/v1/embeddings"
+
+
+class CreateEmbeddingsRequest(BaseModel):
+    youtube: bool
+    blog: bool
+
 
 # Create an instance of the FastAPI class
 app = FastAPI()
@@ -71,9 +79,8 @@ def parse_title_from_metadata(lines):
             return line.split(":")[1].strip()
 
 
-@lru_cache(maxsize=50)
+@lru_cache(maxsize=200)
 def get_embeddings_from_openai(input_text: str):
-    print("Cache miss. OpenAI API request being made.")
     headers = {
         "Authorization": f"Bearer {open_ai_api_key_secret}",
         "Content-Type": "application/json",
@@ -91,6 +98,15 @@ def insert_blog_metadata_in_database(db_conn, cursor, title, url, embeddings):
     return
 
 
+def insert_video_metadata_in_database(
+    db_conn, cursor, title, url, thumbnail, embeddings
+):
+    query = "INSERT INTO content (title, url, category, thumbnail, blog_embedding) VALUES (%s, %s, 1, %s, %s);"
+    cursor.execute(query, (title, url, thumbnail, embeddings))
+    db_conn.commit()
+    return
+
+
 def get_top_recommendations(query_string):
     try:
         db_conn = get_db_conn()
@@ -98,7 +114,7 @@ def get_top_recommendations(query_string):
         query_string_embedding = get_embeddings_from_openai(query_string)
 
         query = """
-            SELECT title, url, (1 - (blog_embedding <=> %s)) AS scores  FROM content ORDER BY scores DESC LIMIT 3;
+            SELECT title, url, (1 - (blog_embedding <=> %s)) AS scores, category, thumbnail  FROM content ORDER BY scores DESC LIMIT 10;
         """
         cursor.execute(query, (str(query_string_embedding),))
 
@@ -114,7 +130,7 @@ def update_blog_metadata_db():
         db_conn = get_db_conn()
         cursor = db_conn.cursor()
         for file_name in extract_files_from_directory(DIRECTORY_PATH):
-            print(f"Processing file: {file_name}")
+
             with open(generate_file_path(file_name), "r", encoding="utf-8") as file:
                 md_content = file.read()
                 url = parse_url_from_metadata(file.name)
@@ -132,6 +148,48 @@ def update_blog_metadata_db():
         close_db_conn(db_conn, cursor)
 
 
+def create_youtube_embeddings():
+    print("Starting processing videos...")
+    keep_parsing = True
+    next_url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUDankIVMXJEkhtjv5yLSN4g&key={youtube_api_key}"
+
+    while keep_parsing:
+        response = requests.get(next_url).json()
+
+        next_page_token = response.get("nextPageToken", None)
+        if next_page_token:
+            paginated_url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUDankIVMXJEkhtjv5yLSN4g&key={youtube_api_key}&pageToken={next_page_token}"
+            next_url = paginated_url
+
+        if not next_page_token:
+            keep_parsing = False
+
+        items = response["items"]
+        for item in items:
+            title = item["snippet"]["title"]
+            description = item["snippet"]["description"]
+            thumbnail = item["snippet"]["thumbnails"]["high"]["url"]
+            video_id = item["snippet"]["resourceId"]["videoId"]
+            video_url = f"https://www.youtube.com/watch?v={video_id}"
+            content_to_embed = f"Title: {title} \n Description: {description}"
+
+            try:
+                db_conn = get_db_conn()
+                cursor = db_conn.cursor()
+                embeddings = get_embeddings_from_openai(content_to_embed)
+
+                insert_video_metadata_in_database(
+                    db_conn=db_conn,
+                    cursor=cursor,
+                    title=title,
+                    url=video_url,
+                    thumbnail=thumbnail,
+                    embeddings=embeddings,
+                )
+            finally:
+                close_db_conn(db_conn, cursor)
+
+
 @app.get("/recommendations")
 def get_users_top_recommendations(
     user_query: str = Query(default="Programming", max_length=100)
@@ -139,9 +197,13 @@ def get_users_top_recommendations(
     return {"data": get_top_recommendations(user_query)}
 
 
-@app.get("/create_emeddings")
-def create_embeddings():
-    update_blog_metadata_db()
+@app.post("/create_embeddings")
+def create_embeddings(request: CreateEmbeddingsRequest):
+    if request.youtube:
+        create_youtube_embeddings()
+    if request.blog:
+        update_blog_metadata_db()
+
     return {"status": "success"}
 
 
